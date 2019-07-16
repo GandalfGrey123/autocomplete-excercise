925e504b13250f75934e840c0aad2f0d38ac2ec0
Marcus <gandalfgrey@gmx.com>
2019-07-16 14:45:22 -0700

    Update App.js

--- application/src/App.js
+++ application/src/App.js
@@ -1,8 +1,11 @@
 import React , { Component } from 'react';
+import SearchForm from './modules/autosearch/search-form'
 
 class App extends Component{
+
   render(){
    return(
+   	<SearchForm/>
    );
   }
 }
